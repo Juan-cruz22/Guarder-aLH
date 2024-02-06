3c446177a2b0ff18daf3cf4371e66f45bf8cb115
Juan-cruz22 <ghigliani02@gmail.com>
2024-02-06 20:52:14 -0300

    commitNetlify

--- src/Banner/Banner.jsx
+++ src/Banner/Banner.jsx
@@ -5,7 +5,7 @@ export default function Banner() {
 
     return (
         <div className={style.conteinerBanner}>
-        <img className={style.imgBanner} src="../../images/BANNER.jpg"></img>
+        <img className={style.imgBanner} src="../../public/BANNER.jpg"></img>
             <div className={style.conteinertext}>
                 <div className={style.conteinerStars}>
                     <img className={style.imgStars} src="../../images/STARS.png"></img>
