ab558ad72e6456f0c9106fe26d5e0b7577f0d92f
Juan-cruz22 <ghigliani02@gmail.com>
2024-02-06 21:05:29 -0300

    commitPhotos

--- src/Banner/Banner.jsx
+++ src/Banner/Banner.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import style from "./Banner.module.css"
+import BANNER from "../../images/BANNER.jpg"
 
 export default function Banner() {
 
     return (
         <div className={style.conteinerBanner}>
-        <img className={style.imgBanner} src="../../images/BANNER.jpg" alt="banner"></img>
+        <img className={style.imgBanner} src={BANNER} alt="banner"></img>
             <div className={style.conteinertext}>
                 <div className={style.conteinerStars}>
                     <img className={style.imgStars} src="../../images/STARS.png"></img>
